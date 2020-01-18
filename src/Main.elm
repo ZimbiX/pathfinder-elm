a8f633c5afd41fe783462e47e244534ae9c09712
ZimbiX <brendan@weibrecht.net.au>
2020-01-18 18:37:50 +1100

    Display logo and scripts to setup and start the app

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, img, text)
+import Html.Attributes exposing (href, src)
 import Html.Events exposing (onClick)
+import Svg exposing (image, svg)
+import Svg.Attributes exposing (height, viewBox, width)
 
 
 
@@ -50,6 +53,12 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ text model
+        [ --[ svg [ width "120", height "120", viewBox "0 0 120 120" ]
+          --    [ image [ href "/icon128.png" ] []
+          --    ]
+          div []
+            [ img [ src "/icon128.png" ] []
+            ]
+        , text model
         , button [ onClick PostMessage ] [ text "Reply" ]
         ]
