fdaf1221dc836d436077402abdf75778c3635986
Brendan Weibrecht <brendan@weibrecht.net.au>
2020-04-30 21:58:27 +1000

    Add curl aliases script

---
+++ curl-aliases.sh
@@ -0,0 +1,20 @@
+id=d
+
+reset() {
+  curl -sSf 'www.zimbico.net/pathfinder-elm-backend/db-setup.php?reset'
+}
+
+move() {
+  local direction="$1"
+  curl -sSf -i \
+    www.zimbico.net/pathfinder-elm-backend/pathfinder-elm-backend.php \
+    -d id="$id" \
+    -d event='{"name": "'"$direction"'", "data": {}}' \
+    -d version=$((++i))
+}
+
+alias w='move MoveUp'
+alias a='move MoveLeft'
+alias s='move MoveDown'
+alias d='move MoveRight'
+alias r='reset'
